9082c9302274e33b28165bc0b0106c4b790354f7
rahulmittapalli <mittapalli.rahul@gmail.com>
2019-02-18 19:10:45 +0530

    removed unnecessary files

--- src/test/java/trialGroupID/Bioluminc.java
+++ src/test/java/trialGroupID/Bioluminc.java
@@ -1,13 +1,13 @@
+package trialGroupID;
 
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class bioluminc {
     @Test
@@ -18,30 +18,28 @@ public class bioluminc {
         WebDriver driver;
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--use-fake-ui-for-media-stream=1");
-        options.addArguments("--use-file-for-fake-video-capture=/users/rahulmittapalli/Downloads/example.y4m");
+        options.addArguments("--use-file-for-fake-video-capture=/users/rahulmittapalli/Downloads/Facemap1.y4m");
         options.addArguments("--disable-user-media-security");
-        options.addArguments("--use-fake-ui-for-media-stream");
         options.addArguments("--use-fake-device-for-media-stream");
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("media.default_video_capture_Device", "\\\\?\\root#media#0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global");
-        options.setExperimentalOption("prefs", prefs);
         driver = new ChromeDriver(options);
         driver.get("https://www.biolumincmirror.com/");
         driver.manage().window().maximize();
-        Thread.sleep(5000);
-        driver.findElement(By.id("getStarted")).click();
+        WebDriverWait wait =new  WebDriverWait(driver, 10); 
+        By addItem = By.id("getStarted");
+        wait.until(ExpectedConditions.presenceOfElementLocated(addItem)).click();
         System.out.println("Clicked");
         Thread.sleep(15000);
         WebElement camera=driver.findElement(By.id("_drawing"));
         System.out.println("Camera is Displayed "+camera.isDisplayed());
         WebElement information=driver.findElement(By.id("_imageData"));
         System.out.println("Image is Displayed "+information.isDisplayed());
-        Thread.sleep(5000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement product= driver.findElement(By.xpath("//*[@class='m_0 pt_15 LTMD_font']"));
         System.out.println("Product name is "+product.getText());
         String Name="NEW! biolumin-c serum";
         Assert.assertEquals(Name,product.getText());
         WebElement Button=driver.findElement(By.id("mainproduct"));
+        js.executeScript("arguments[0].scrollIntoView();",product );
         System.out.println("Button visibility is "+Button.isDisplayed());
         Thread.sleep(5000);
         driver.close();
